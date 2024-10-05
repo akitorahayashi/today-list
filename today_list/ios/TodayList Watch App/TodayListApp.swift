4dfedacf4a75421d98ef6e7a282d8b7c6cd5d3f3
林明虎 <hayashi20030703@gmail.com>
2024-10-05 15:04:07 +0900

    watchOSアプリを追加し、Hello worldした

---
+++ today_list/ios/TodayList Watch App/TodayListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayListApp.swift
+//  TodayList Watch App
+//
+//  Created by 林 明虎 on 2024/10/05.
+//
+
+import SwiftUI
+
+@main
+struct TodayList_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
