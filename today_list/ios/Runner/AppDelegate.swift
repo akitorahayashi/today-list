d613eadd0819ea3ee8eb3b3785f92e890a3db4c1
林明虎 <hayashi20030703@gmail.com>
2024-10-21 22:18:18 +0900

    Recommit

--- today_list/ios/Runner/AppDelegate.swift
+++ today_list/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import WidgetKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,48 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller = window?.rootViewController as! FlutterViewController
+        
+        let userdefaults = UserDefaults(suiteName: "group.akitorahayashi.todayListGroup")
+        
+        // --- showTodosInAWorkspaceWidget
+        let showTodosInAWorkspaceWidgetMethodChannel = FlutterMethodChannel(name: "com.akitora0703.todaylist/show_todos_in_a_workspace_widget", binaryMessenger: controller.binaryMessenger)
+        
+        showTodosInAWorkspaceWidgetMethodChannel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            switch call.method {
+            case "updateTLWorkspaces":
+                if let stringOfStringWorkspaces = call.arguments as? String {
+                    userdefaults?.set(stringOfStringWorkspaces, forKey: "tlWorkspaces") // Fixed typo
+                    // メインスレッドでの更新を追加
+                    DispatchQueue.main.async {
+                        WidgetCenter.shared.reloadAllTimelines()
+                    }
+                    result("workspaces saved successfully")
+                } else {
+                    let errorMessage = "Expected a String for updateTLWorkspaces"
+                    print(errorMessage) // Log error
+                    result(FlutterError(code: "INVALID_ARGUMENT", message: errorMessage, details: nil))
+                }
+                
+            case "updateSelectedTheme":
+                if let selectedTheme = call.arguments as? String {
+                    userdefaults?.set(selectedTheme, forKey: "selectedTheme")
+                    // メインスレッドでの更新を追加
+                    DispatchQueue.main.async {
+                        WidgetCenter.shared.reloadAllTimelines()
+                    }
+                    result("selectedTheme saved successfully")
+                } else {
+                    let errorMessage = "Expected a String for updateSelectedTheme"
+                    print(errorMessage) // Log error
+                    result(FlutterError(code: "INVALID_ARGUMENT", message: errorMessage, details: nil))
+                }
+                
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
+        
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
