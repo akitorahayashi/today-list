89fdb66b6f6a78e0b6dfd8a66ede686af7aa4ab1
林明虎 <hayashi20030703@gmail.com>
2024-11-02 13:00:08 +0900

    currentWorkspaceIndexProviderの内容をcurrentWorkspaceProviderに統合した

--- today_list/lib/model/workspace/current_workspace_provider.dart
+++ today_list/lib/model/workspace/current_workspace_provider.dart
@@ -2,35 +2,94 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../external/tl_pref.dart';
 import './tl_workspace.dart';
 import './tl_workspaces_provider.dart';
+import '../todo/tl_category.dart';
 
 // currentWorkspaceを提供するProvider
-final currentWorkspaceProvider = Provider.autoDispose<TLWorkspace>((ref) {
-  final _tlWorkspaces = ref.watch(tlWorkspacesProvider);
-  final _correntIndex = ref.watch(currentWorkspaceIndexProvider);
-  return _tlWorkspaces[_correntIndex];
-});
-
-// currentWorkspaceIndexを提供するProvider
-final currentWorkspaceIndexProvider =
-    StateNotifierProvider.autoDispose<CurrentWorkspaceIndexNotifier, int>(
+final currentWorkspaceProvider =
+    StateNotifierProvider.autoDispose<CurrentWorkspaceNotifier, TLWorkspace>(
         (ref) {
-  return CurrentWorkspaceIndexNotifier();
+  final _tlWorkspaces = ref.watch(tlWorkspacesProvider);
+  return CurrentWorkspaceNotifier(ref, _tlWorkspaces);
 });
 
-// currentWorkspaceIndexを管理するNotifier
-class CurrentWorkspaceIndexNotifier extends StateNotifier<int> {
-  CurrentWorkspaceIndexNotifier() : super(0) {
-    // コンストラクタ
+// currentWorkspaceを管理するNotifier
+class CurrentWorkspaceNotifier extends StateNotifier<TLWorkspace> {
+  final Ref ref;
+  int currentWorkspaceIndex;
+
+  CurrentWorkspaceNotifier(this.ref, List<TLWorkspace> workspaces)
+      : currentWorkspaceIndex = 0,
+        super(workspaces[0]) {
+    // 初期化処理
     TLPref().getPref.then((pref) {
       final initialIndex = pref.getInt('currentWorkspaceIndex') ?? 0;
-      state = initialIndex;
+      currentWorkspaceIndex = initialIndex;
+      state = workspaces[currentWorkspaceIndex];
     });
   }
 
-  Future<void> changeCurrentWorkspaceIndex(int newIndex) async {
-    state = newIndex;
+  // 現在のワークスペースインデックスを変更する関数
+  Future<void> changeCurrentWorkspaceIndex(int newCurrentWorkspaceIndex) async {
+    currentWorkspaceIndex = newCurrentWorkspaceIndex;
+    state = ref.read(tlWorkspacesProvider)[currentWorkspaceIndex];
     await TLPref().getPref.then((pref) {
-      pref.setInt('currentWorkspaceIndex', newIndex);
+      pref.setInt('currentWorkspaceIndex', newCurrentWorkspaceIndex);
     });
+  }
+
+  // 現在のworkspaceの今日でチェック済みtodoを全て削除するための関数
+  Future<void> deleteCheckedToDosInTodayInAWorkspace() async {
+    final tlWorkspacesNotifier = ref.read(tlWorkspacesProvider.notifier);
+    final TLWorkspace selectedWorkspace = state;
+
+    for (TLCategory bigCategory in selectedWorkspace.bigCategories) {
+      // bigCategoryに関するcheckのsort
+      deleteAllCheckedToDosInThisToDos(
+        onlyToday: true,
+        selectedWorkspaceIndex: currentWorkspaceIndex,
+        selectedWorkspace: selectedWorkspace,
+        selectedToDos: selectedWorkspace.toDos[bigCategory.id]!,
+      );
+      for (TLCategory smallCategory
+          in selectedWorkspace.smallCategories[bigCategory.id]!) {
+        // smallCategoryに関するcheckのsort
+        deleteAllCheckedToDosInThisToDos(
+            onlyToday: true,
+            selectedWorkspaceIndex: currentWorkspaceIndex,
+            selectedWorkspace: selectedWorkspace,
+            selectedToDos: selectedWorkspace.toDos[smallCategory.id]!);
+      }
+    }
+
+    void deleteAllCheckedToDosInThisToDos({
+      required bool onlyToday,
+      int? selectedWorkspaceIndex,
+      TLWorkspace? selectedWorkspace,
+      required TLToDos selectedToDos,
+    }) {
+      // effortなどを更新する
+      // ToDos.countDeletedToDo(
+      //     selectedWorkspaceIndex: selectedWorkspaceIndex,
+      //     selectedWorkspace: selectedWorkspace,
+      //     numberOfDeletedToDo: selectedToDos.countCheckedToDosAndStepsInThisToDos(
+      //         selectedToDos: selectedToDos));
+      // チェックされているtodoを消す
+      selectedToDos.toDosInToday.removeWhere((todo) => todo.isChecked);
+      if (!onlyToday) {
+        selectedToDos.toDosInWhenever.removeWhere((todo) => todo.isChecked);
+      }
+      // 残っているものでstepがチェックされているものを消す
+      for (TLToDo todo in selectedToDos.toDosInToday) {
+        todo.steps.removeWhere((step) => step.isChecked);
+      }
+      for (TLToDo todo in selectedToDos.toDosInWhenever) {
+        todo.steps.removeWhere((step) => step.isChecked);
+      }
+      // ここでセーブとかvibrateしたら複数になることがある
+    }
+
+    // 更新されたワークスペースを保存
+    tlWorkspacesNotifier.updateWorkspace(
+        currentWorkspaceIndex, selectedWorkspace);
   }
 }
