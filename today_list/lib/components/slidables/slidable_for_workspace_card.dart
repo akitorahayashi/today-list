1f6f2ed260cdbf4c8c6e7d1c7f7ab08f34682ee5
林明虎 <hayashi20030703@gmail.com>
2024-11-03 17:43:53 +0900

    SlidableForWorkspaceCardにConsumerWidgetを継承させて再度実装し直した

--- today_list/lib/components/slidables/slidable_for_workspace_card.dart
+++ today_list/lib/components/slidables/slidable_for_workspace_card.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../model/workspace/current_tl_workspace_provider.dart';
-import '../../model/workspace/tl_workspace.dart';
-import '../../model/workspace/tl_workspaces_provider.dart';
-import '../../components/dialog/common/yes_no_dialog.dart';
+import '../../components/dialog/for_workspace/add_or_edit_workspace_dialog.dart';
+import '../../components/dialog/for_workspace/delete_workspace_dialog.dart';
+import '../../model/tl_workspace.dart';
+import '../../model/provider/tl_workspaces_provider.dart';
 import '../../model/design/tl_theme.dart';
 
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 class SlidableForWorkspaceCard extends ConsumerWidget {
-  final bool isInDrawerList;
   final bool isCurrentWorkspace;
   final int indexInTLWorkspaces;
   final Widget child;
   const SlidableForWorkspaceCard({
     super.key,
-    required this.isInDrawerList,
     required this.isCurrentWorkspace,
     required this.indexInTLWorkspaces,
     required this.child,
@@ -25,46 +23,31 @@ class SlidableForWorkspaceCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final TLThemeData _tlThemeData = TLTheme.of(context);
     // provider
-    final TLWorkspace _currentTLWorkspace =
-        ref.watch(currentTLWorkspaceProvider);
-    // notifier
-    final TLWorkspacesNotifier _tlWorkspacesNotifier =
-        ref.read(tlWorkspacesProvider.notifier);
-    final CurrentTLWorkspaceNotifier _currentTLWorkspaceNotifier =
-        ref.read(currentTLWorkspaceProvider.notifier);
+    final List<TLWorkspace> _tlWorkspaces = ref.watch(tlWorkspacesProvider);
+    // other
+    final TLWorkspace _corrWorkspace = _tlWorkspaces[indexInTLWorkspaces];
     return Slidable(
       // currentWorkspaceの時や
-      startActionPane: widget.isCurrentWorkspace ||
+      startActionPane: isCurrentWorkspace ||
               // デフォルトワークスペースの時は削除できないようにする
-              (widget.indexInTLWorkspaces == 0)
+              indexInTLWorkspaces == 0
           ? null
           : ActionPane(
               motion: const ScrollMotion(),
               extentRatio: 0.35,
               children: [
+                // For deleting workspace
                 SlidableAction(
                   autoClose: true,
                   spacing: 8,
                   backgroundColor: _tlThemeData.panelColor,
                   foregroundColor: _tlThemeData.accentColor,
-                  onPressed: (BuildContext context) {
-                    if (!widget.isInDrawerList) {
-                      Navigator.pop(context);
-                    }
-                    showDialog(
+                  onPressed: (BuildContext context) async {
+                    await showDialog(
                         context: context,
-                        builder: (context) => YesNoDialog(
-                            title: title,
-                            message: message,
-                            yesAction: () async {
-                              await deleteWorkspaceAlert(
-                                  context: context,
-                                  indexInTLWorkspaces:
-                                      widget.indexInTLWorkspaces);
-                            }));
-                    deleteWorkspaceAlert(
-                        context: context,
-                        indexInTLWorkspaces: widget.indexInTLWorkspaces);
+                        builder: (context) => DeleteWorkspaceDialog(
+                            corrWorkspaceIndex: indexInTLWorkspaces,
+                            willDeletedWorkspace: _corrWorkspace));
                   },
                   icon: Icons.remove,
                   label: "Delete",
@@ -72,7 +55,7 @@ class SlidableForWorkspaceCard extends ConsumerWidget {
               ],
             ),
       endActionPane: // デフォルトワークスペースの時は編集できないようにする
-          widget.indexInTLWorkspaces == 0
+          indexInTLWorkspaces == 0
               ? null
               : ActionPane(
                   motion: const ScrollMotion(),
@@ -84,25 +67,21 @@ class SlidableForWorkspaceCard extends ConsumerWidget {
                       backgroundColor: _tlThemeData.panelColor,
                       foregroundColor: _tlThemeData.accentColor,
                       onPressed: (BuildContext context) async {
-                        if (!widget.isInDrawerList) {
-                          Navigator.pop(context);
-                        }
                         await showDialog(
                             context: context,
                             barrierDismissible: false,
                             builder: (context) {
                               return AddOrEditWorkspaceDialog(
                                   oldIndexInStringWorkspaces:
-                                      widget.indexInTLWorkspaces);
+                                      indexInTLWorkspaces);
                             });
-                        setState(() {});
                       },
                       icon: Icons.edit,
                       label: "Edit",
                     ),
                   ],
                 ),
-      child: widget.child,
+      child: child,
     );
   }
 }
