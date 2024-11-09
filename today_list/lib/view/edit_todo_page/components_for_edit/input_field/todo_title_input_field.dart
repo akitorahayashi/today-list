35060ce89b0fc95be65dc488be0278b5de8b2741
林明虎 <hayashi20030703@gmail.com>
2024-11-09 22:05:15 +0900

    ToDo追加時にSnackBarを表示するようにした

--- today_list/lib/view/edit_todo_page/components_for_edit/input_field/todo_title_input_field.dart
+++ today_list/lib/view/edit_todo_page/components_for_edit/input_field/todo_title_input_field.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:today_list/components/snack_bar/snack_bar_to_notify_todo_or_step_is_edited.dart';
+import 'package:today_list/model/external/tl_vibration.dart';
 import '../../../../model/editing_provider/editing_todo_provider.dart';
 import '../../../../model/design/tl_theme.dart';
 import '../../../../styles/styles.dart';
@@ -52,7 +54,15 @@ class ToDoTitleInputFieldState extends ConsumerState<ToDoTitleInputField> {
               child: TextButton(
                 onPressed: _enteredToDoTitle.trim().isEmpty
                     ? null
-                    : () => editingToDoNotifier.completeEditting(),
+                    : () {
+                        NotifyTodoOrStepIsEditedSnackBar.show(
+                            context: context,
+                            newTitle: _enteredToDoTitle,
+                            newCheckedState: false,
+                            quickChangeToToday: null);
+                        TLVibration.vibrate();
+                        editingToDoNotifier.completeEditting();
+                      },
                 child: Icon(
                   Icons.add,
                   color: _enteredToDoTitle.trim().isNotEmpty
