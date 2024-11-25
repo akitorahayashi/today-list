056e918739909d5799ca4b1aee22f97e1802bdaf
akitora <hayashi20030703@gmail.com>
2024-11-25 11:12:38 +0900

    アプリアイコンを変更するにあたって、changeIcon関数とinfo.plistを整備した

--- lib/model/user/setting_data.dart
+++ lib/model/user/setting_data.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_dynamic_icon/flutter_dynamic_icon.dart';
+import 'package:today_list/model/tl_theme.dart';
 
 class SettingData {
   static Future<void> changeIcon(String? iconName) async {
     try {
       if (await FlutterDynamicIcon.supportsAlternateIcons) {
-        await FlutterDynamicIcon.setAlternateIconName(iconName);
-        print('Icon changed to: $iconName');
+        if (iconName == tlThemeDataList[0].themeName) {
+          print('Icon changed to: $iconName');
+          await FlutterDynamicIcon.setAlternateIconName(null);
+        } else {
+          print('Icon changed to: $iconName');
+          await FlutterDynamicIcon.setAlternateIconName(iconName);
+        }
       } else {
         print("Dynamic icon change is not supported on this device.");
       }
