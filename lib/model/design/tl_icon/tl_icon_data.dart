69d52481bb5f723975c62f632917340382384b57
akitora <hayashi20030703@gmail.com>
2025-01-26 15:00:06 +0900

    選択しているチェックボックスのアイコンの情報をTLIconDataクラスにまとめ独立させた

--- lib/model/design/tl_icon/tl_icon_data.dart
+++ lib/model/design/tl_icon/tl_icon_data.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 /// このファイルには、アイコンに関連する列挙型やデータモデルを定義する。
 /// アイコン情報のデータ構造を統一し、アプリ全体で再利用可能にすることを目的としている。
 
+class TLIconData {
+  final TLIconCategory category;
+  final TLIconRarity rarity;
+  final TLIconName name;
+
+  TLIconData({
+    required this.category,
+    required this.rarity,
+    required this.name,
+  });
+
+  Map<String, String> toJson() => {
+        'category': category.name,
+        'rarity': rarity.name,
+        'name': name.name,
+      };
+
+  static TLIconData fromJson(Map<String, dynamic> json) => TLIconData(
+        category: TLIconCategory.values.firstWhere(
+            (e) => e.name == json['category'],
+            orElse: () => TLIconCategory.defaultCategory),
+        rarity: TLIconRarity.values.firstWhere((e) => e.name == json['rarity'],
+            orElse: () => TLIconRarity.common),
+        name: TLIconName.values.firstWhere((e) => e.name == json['name'],
+            orElse: () => TLIconName.box),
+      );
+}
+
 /// カテゴリー（大カテゴリー）
 enum TLIconCategory { defaultCategory, unit1, unit2 }
 
--- lib/view_model/design/tl_icon_data_provider.dart
+++ lib/view_model/design/tl_icon_data_provider.dart
@@ -1,41 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:today_list/resource/tl_icon_resource.dart';
 import '../../service/tl_pref.dart';
-import '../../model/design/tl_icon_model.dart';
+import '../../model/design/tl_icon/tl_icon_data.dart';
 import 'dart:convert';
 
 final tlIconDataProvider =
     StateNotifierProvider<TLIconDataNotifier, TLIconData>(
   (ref) => TLIconDataNotifier(),
 );
-
-class TLIconData {
-  final TLIconCategory category;
-  final TLIconRarity rarity;
-  final TLIconName name;
-
-  TLIconData({
-    required this.category,
-    required this.rarity,
-    required this.name,
-  });
-
-  Map<String, String> toJson() => {
-        'category': category.name,
-        'rarity': rarity.name,
-        'name': name.name,
-      };
-
-  static TLIconData fromJson(Map<String, dynamic> json) => TLIconData(
-        category: TLIconCategory.values.firstWhere(
-            (e) => e.name == json['category'],
-            orElse: () => TLIconCategory.defaultCategory),
-        rarity: TLIconRarity.values.firstWhere((e) => e.name == json['rarity'],
-            orElse: () => TLIconRarity.common),
-        name: TLIconName.values.firstWhere((e) => e.name == json['name'],
-            orElse: () => TLIconName.box),
-      );
-}
 
 class TLIconDataNotifier extends StateNotifier<TLIconData> {
   TLIconDataNotifier()
