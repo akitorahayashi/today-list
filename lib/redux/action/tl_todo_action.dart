118fae2eb07415f9eb490600fea868b584781850
akitora <hayashi20030703@gmail.com>
2025-02-03 10:36:48 +0900

    editing_todo_provider.dartを参考にしてTLToDoActionを作成した

---
+++ lib/redux/action/tl_todo_action.dart
@@ -0,0 +1,34 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:today_list/model/todo/tl_todo.dart';
+import 'package:today_list/model/todo/tl_step.dart';
+
+part 'generate/tl_todo_action.freezed.dart';
+
+// $ flutter pub run build_runner build
+
+@freezed
+class TLToDoAction with _$TLToDoAction {
+  /// ToDo の編集を開始する
+  const factory TLToDoAction.startEditingToDo({
+    required String categoryId,
+    required String? smallCategoryId,
+    required int indexOfEditingToDo,
+    required bool ifInToday,
+  }) = StartEditingToDoAction;
+
+  /// ToDo のタイトルを更新
+  const factory TLToDoAction.updateToDoTitle({
+    required String newTitle,
+  }) = UpdateToDoTitleAction;
+
+  /// ToDo のステップを追加
+  const factory TLToDoAction.addStep({
+    required String stepTitle,
+  }) = AddStepAction;
+
+  /// ToDo の編集を完了する
+  const factory TLToDoAction.completeEditing() = CompleteEditingToDoAction;
+
+  /// 編集中のToDoをリセット（キャンセル）
+  const factory TLToDoAction.resetEditingToDo() = ResetEditingToDoAction;
+}
