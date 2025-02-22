2ecb40575c203301032c143134db5c7bdf32645c
akitorahayashi <atora.has75@gmail.com>
2025-02-23 00:14:31 +0900

    HomePageの内容を整理した

---
+++ lib/view/page/home_page/tab_content/todo_list_in_workspace_in_today_and_whenever.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:today_list/model/todo/tl_workspace.dart';
+import 'package:today_list/util/tl_workspace_utils.dart';
+import 'package:today_list/view/page/home_page/build_todo_list/list_of_category_to_todos.dart';
+import 'package:today_list/view/page/home_page/build_todo_list/num_todos_card.dart';
+
+class ToDoListInWorkspaceInTodayAndWhenever extends StatelessWidget {
+  final TLWorkspace corrWorkspace;
+
+  const ToDoListInWorkspaceInTodayAndWhenever({required this.corrWorkspace});
+
+  @override
+  Widget build(BuildContext context) {
+    final numOfToDosInToday =
+        TLWorkspaceUtils.getNumOfToDo(corrWorkspace, ifInToday: true);
+    final numOfToDosInWhenever =
+        TLWorkspaceUtils.getNumOfToDo(corrWorkspace, ifInToday: false);
+
+    return ListView(
+      key: PageStorageKey(corrWorkspace.id), // スクロール位置を保持
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: NumToDosCard(title: null, numTodos: numOfToDosInToday),
+        ),
+        ListOfCategoryToToDos(ifInToday: true, corrWorkspace: corrWorkspace),
+        if (numOfToDosInWhenever != 0)
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: NumToDosCard(
+                title: "In Whenever", numTodos: numOfToDosInWhenever),
+          ),
+        ListOfCategoryToToDos(ifInToday: false, corrWorkspace: corrWorkspace),
+        const SizedBox(height: 250),
+      ],
+    );
+  }
+}
