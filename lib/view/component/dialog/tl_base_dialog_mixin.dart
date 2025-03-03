4d265482310bea5eac54ff791d3e456af32c975c
akitorahayashi <atora.has75@gmail.com>
2025-03-03 10:51:04 +0900

    ダイアログにアニメーションを適用した

--- lib/view/component/dialog/tl_base_dialog_mixin.dart
+++ lib/view/component/dialog/tl_base_dialog_mixin.dart
@@ -2,14 +2,32 @@ import 'package:flutter/material.dart';
 
 // stateless dialog
 mixin TLBaseDialogMixin {
-  Future<void> show({
+  Future<Object?> show({
     required BuildContext context,
   }) async {
-    return showDialog(
+    return showGeneralDialog(
       context: context,
       barrierDismissible: false,
-      builder: (context) {
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      barrierColor: Colors.black54,
+      transitionDuration: const Duration(milliseconds: 200),
+      pageBuilder: (BuildContext buildContext, Animation animation,
+          Animation secondaryAnimation) {
         return this as Widget;
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeTransition(
+          opacity: animation,
+          child: ScaleTransition(
+            scale: Tween<double>(begin: 0.85, end: 1.0).animate(
+              CurvedAnimation(
+                parent: animation,
+                curve: Curves.easeOut,
+              ),
+            ),
+            child: child,
+          ),
+        );
       },
     );
   }
